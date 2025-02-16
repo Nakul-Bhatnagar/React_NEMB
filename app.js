9a7a8e7f4f0ff021a11312fc3fc408af89626c2a
Nakul-Bhatnagar <nakulbhatnagar0212@gmail.com>
2025-02-16 23:37:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const {addition , substraction , multiplication , division} = require("./mathOperations")
+
+console.log(addition(10,20));
+console.log(substraction(20,5));
+console.log(multiplication(6,6));
+console.log(division(10,2));
